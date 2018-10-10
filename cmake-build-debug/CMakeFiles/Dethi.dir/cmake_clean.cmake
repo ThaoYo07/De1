6d6a40a953642e35fad49c468cfff049ef664940
Thao <thaontpth1809022@fpt.edu.vn>
2018-10-10 19:11:05 +0700

    Nop bai

---
+++ cmake-build-debug/CMakeFiles/Dethi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dethi.dir/main.c.o"
+  "Dethi.pdb"
+  "Dethi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dethi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tinhChuViTamGiac(int a, int b, int c) {
+    return (a + b + c) / 2;
+}
+int tong(int i, int j){
+    return i + j;
+}
+
+int main() {
+    int a, b, c, tongab, tongbc, tongac;
+    printf("Vui long nhap so do 3 canh cua tam giac\n");
+    printf("Canh thu nhat la : ");
+    scanf("%d", &a);
+    printf("Canh thu hai la : ");
+    scanf("%d", &b);
+    printf("Canh thu ba la : ");
+    scanf("%d", &c);
+    tongab = a + b;
+    tongac = a + c;
+    tongbc = c + b;
+    if (a <= 0) {
+        printf("Độ dài một cạnh tam giác không thể nhỏ hơn hoặc bằng 0.");
+        exit(1);
+    } else if (b <= 0) {
+        printf("Độ dài một cạnh tam giác không thể nhỏ hơn hoặc bằng 0.");
+        exit(1);
+    } else if (c <= 0) {
+        printf("Độ dài một cạnh tam giác không thể nhỏ hơn hoặc bằng 0.");
+        exit(1);
+    } else if (tongab <= c){
+        printf("Tổng hai cạnh của một tam giác luôn phải lớn hơn cạnh còn lại (Theo bất đẳng thức tam giác)");
+        exit(1);
+    } else if (tongac <= b){
+        printf("Tổng hai cạnh của một tam giác luôn phải lớn hơn cạnh còn lại (Theo bất đẳng thức tam giác)");
+        exit(1);
+    } else if (tongbc <= a){
+        printf("Tổng hai cạnh của một tam giác luôn phải lớn hơn cạnh còn lại (Theo bất đẳng thức tam giác)");
+        exit(1);
+    } else {
+        tinhChuViTamGiac(a, b, c);
+        printf("Nua chu vi tam giac la: %d", tinhChuViTamGiac(a, b, c));
+    }
+
+return 0;
+}
